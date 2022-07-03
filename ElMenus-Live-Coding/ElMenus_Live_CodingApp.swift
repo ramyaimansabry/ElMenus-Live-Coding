3aa393408c391f300ce80bd0799ab4a8ed276c68
Ramy Sabry <ramysabry1996@gmail.com>
2022-07-03 19:04:27 +0200

    Initial Commit

---
+++ ElMenus-Live-Coding/ElMenus_Live_CodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElMenus_Live_CodingApp.swift
+//  ElMenus-Live-Coding
+//
+//  Created by Ramy Sabry on 03/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ElMenus_Live_CodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
